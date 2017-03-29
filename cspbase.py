6aa1ed8ff2b928124e40ebc314ad7d5b37934a4c
Abe Ratnofsky <aratno@gmail.com>
2017-03-29 02:15:34 -0400

    Replaced satisfying tuples tables with predicate; need to vet dependents

--- cspbase.py
+++ cspbase.py
@@ -17,23 +17,23 @@ import functools
 
     B) class constraint
 
-      This class allows one to define constraints specified by tables
-      of satisfying assignments.
+      This class allows one to define constraints specified by variable
+      scopes and predicate functions. The predicate returns True if the
+      values for the variables in scope are valid assignments, and False
+      otherwise. This costs more memory than the table of satisfying
+      assignments method provided in the A2 starter code, but is unable
+      to support GAC.
 
       On initialization the variables the constraint is over is
-      specified (i.e. the scope of the constraint). This must be an
-      ORDERED list of variables. This list of variables cannot be
-      changed once the constraint object is created.
+      specified (i.e. the scope of the constraint), as well as the predicate
+      function that takes the elements of the scope as arguments in order.
 
-      Once initialized the constraint can be incrementally initialized
-      with a list of satisfying tuples. Each tuple specifies a value
-      for each variable in the constraint (in the same ORDER as the
-      variables of the constraint were specified).
+      Because the constraints are implemented as predicates, no satisfying
+      tuples must be added.
 
     C) Backtracking routine---takes propagator and CSP as arguments
        so that basic backtracking, forward-checking or GAC can be 
        executed depending on the propagator used.
-
 '''
 
 class Variable: 
@@ -204,45 +204,42 @@ class Constraint:
        the satisfied function which tests if an assignment to the
        variables in the constraint's scope satisfies the constraint'''
 
-    def __init__(self, name, scope): 
+    def __init__(self, name, scope, pred): 
         '''create a constraint object, specify the constraint name (a
-        string) and its scope (an ORDERED list of variable objects).
-        The order of the variables in the scope is critical to the
-        functioning of the constraint.
-
-        Consraints are implemented as storing a set of satisfying
-        tuples (i.e., each tuple specifies a value for each variable
-        in the scope such that this sequence of values satisfies the
-        constraints).
-
-        NOTE: This is a very space expensive representation...a proper
-        constraint object would allow for representing the constraint
-        with a function.  
+        string), its scope (an ORDERED list of variable objects), and
+        the predicate function over its scope.
+        The order of the variables in the scope is the same as their order
+        in the predicate, and is critical to the functioning of the constraint.
         '''
 
         self.scope = list(scope)
         self.name = name
-        self.sat_tuples = dict()
 
-        #The next object data item 'sup_tuples' will be used to help
-        #support GAC propgation. It allows access to a list of 
-        #satisfying tuples that contain a particular variable/value
-        #pair.
-        self.sup_tuples = dict()
+        import inspect
+        # Predicate must accept the right number of variables as arguments
+        if len(inspect.getargspec(pred).args) != len(scope):
+            raise Exception('Constraint predicate arity mismatch: scope contains incorrect number of variables')
+        # Predicate must not be variadic
+        if len(inspect.getargspec(pred).varargs) != 0:
+            raise Exception('Constraint predicate must not be variadic')
 
-    def add_satisfying_tuples(self, tuples):
-        '''We specify the constraint by adding its complete list of satisfying tuples.'''
-        for x in tuples:
-            t = tuple(x)  #ensure we have an immutable tuple
-            if not t in self.sat_tuples:
-                self.sat_tuples[t] = True
+        self.pred = pred
 
-            #now put t in as a support for all of the variable values in it
-            for i, val in enumerate(t):
-                var = self.scope[i]
-                if not (var,val) in self.sup_tuples:
-                    self.sup_tuples[(var,val)] = []
-                self.sup_tuples[(var,val)].append(t)
+    # Removing for predicates over tables
+    #
+    # def add_satisfying_tuples(self, tuples):
+    #     '''We specify the constraint by adding its complete list of satisfying tuples.'''
+    #     for x in tuples:
+    #         t = tuple(x)  #ensure we have an immutable tuple
+    #         if not t in self.sat_tuples:
+    #             self.sat_tuples[t] = True
+
+    #         #now put t in as a support for all of the variable values in it
+    #         for i, val in enumerate(t):
+    #             var = self.scope[i]
+    #             if not (var,val) in self.sup_tuples:
+    #                 self.sup_tuples[(var,val)] = []
+    #             self.sup_tuples[(var,val)].append(t)
 
     def get_scope(self):
         '''get list of variables the constraint is over'''
@@ -255,7 +252,10 @@ class Constraint:
            constraints "satisfies" function.  Note the list of values
            are must be ordered in the same order as the list of
            variables in the constraints scope'''
-        return tuple(vals) in self.sat_tuples
+        # List of vals must be the same size as predicate scope
+        if len(vals) != len(self.get_scope):
+            raise Exception('Constraint check arity mismatch')
+        return self.pred(*vals)
 
     def get_n_unasgn(self):
         '''return the number of unassigned variables in the constraint's scope'''
@@ -274,24 +274,28 @@ class Constraint:
                 vs.append(v)
         return vs
 
-    def has_support(self, var, val):
-        '''Test if a variable value pair has a supporting tuple (a set
-           of assignments satisfying the constraint where each value is
-           still in the corresponding variables current domain
-        '''
-        if (var, val) in self.sup_tuples:
-            for t in self.sup_tuples[(var, val)]:
-                if self.tuple_is_valid(t):
-                    return True
-        return False
+    # TODO: Confirm that this can be safely removed.
+    #
+    # def has_support(self, var, val):
+    #     '''Test if a variable value pair has a supporting tuple (a set
+    #        of assignments satisfying the constraint where each value is
+    #        still in the corresponding variables current domain
+    #     '''
+    #     if (var, val) in self.sup_tuples:
+    #         for t in self.sup_tuples[(var, val)]:
+    #             if self.tuple_is_valid(t):
+    #                 return True
+    #     return False
 
-    def tuple_is_valid(self, t):
-        '''Internal routine. Check if every value in tuple is still in
-           corresponding variable domains'''
-        for i, var in enumerate(self.scope):
-            if not var.in_cur_domain(t[i]):
-                return False
-        return True
+    # TODO: Confirm that this can be safely removed.
+    #
+    # def tuple_is_valid(self, t):
+    #     '''Internal routine. Check if every value in tuple is still in
+    #        corresponding variable domains'''
+    #     for i, var in enumerate(self.scope):
+    #         if not var.in_cur_domain(t[i]):
+    #             return False
+    #     return True
 
     def __str__(self):
         return("{}({})".format(self.name,[var.name for var in self.scope]))
